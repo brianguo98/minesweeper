fdaf3f16477db52bc299a80b141f3fca8781b512
Brian Guo <brianguo@umich.edu>
2020-02-02 17:09:22 -0500

    added replayability

--- python/board.py
+++ python/board.py
@@ -20,15 +20,16 @@ class Board:
         self.solutionBoard = self.setSolBoard([[Cell() for x in range(n)] for y in range(n)], self.dimension, num_mines)
 
     def setSolBoard(self, board, n, num_mines): 
-    # randomly assigns num_mines mines to the n*n board
         sequence = [i for i in range(n*n)]
         mineLocations = set(random.sample(sequence, num_mines))
         counter = 0
+        # set randomized mine locations
         for row in range(n):
             for col in range(n):
                 if counter in mineLocations:
                     board[row][col].placeMine()
                 counter += 1
+        # calculate adjacent mines for cells with them
         for row in range(n):
             for col in range(n):
                 numAdj = self.countAdjacent(board, row, col)
@@ -57,61 +58,69 @@ class Board:
             adjacent.append(((row+adjRow), (col+adjCol)))
         return adjacent
 
-# fix this shit!!!
     def autoOpen(self, row, col, visited):
-    # recursive function to automatically open neighboring non-visited
-    # cells when a cell has no adjacent mines '''
+    # DFS recursive function to automatically open neighboring cells
+        # base case 1, we've already been to this cell
         if visited[row][col]:
             return
+        # base case 2, this cell has mines adjacent to it, can't go any furthur, must backtrack
         if self.solutionBoard[row][col].state != '-':
-            # print('wev ever see this shit?')
             self.playerBoard[row][col].state = self.solutionBoard[row][col].state
             return
         visited[row][col] = True
+        # for every cell adjacent to the current cell
         for (adjRow, adjCol) in self.getAdjacent(row, col):
-            # adjRow and adjCol is valid
+            # check that the adjacent cell is in the grid, not a mine, and hasn't been visited yet
             if (0 <= adjRow and adjRow < self.dimension
                 and 0 <= adjCol and adjCol < self.dimension
                 and not self.solutionBoard[adjRow][adjCol].isMine
                 and not visited[adjRow][adjCol]):
-                if self.solutionBoard[adjRow][adjCol].state == '-':
-                    self.playerBoard[adjRow][adjCol].state = '-'
-                else:
-                    self.playerBoard[adjRow][adjCol].state = self.solutionBoard[adjRow][adjCol]
+                # set to appropriate state
+                self.playerBoard[adjRow][adjCol] = self.solutionBoard[adjRow][adjCol]
+                # recursive call
                 self.autoOpen(adjRow, adjCol, visited)
 
 
     def open(self, row, col):
-        if self.solutionBoard[row][col].isMine:
-            self.playerBoard[row][col].state = 'M'
-            return (False, False)
-        else:
-            if self.solutionBoard[row][col].state == '-':
-                self.playerBoard[row][col].state = '-'
-                self.autoOpen(row, col, [[False for x in range(self.dimension)] for y in range(self.dimension)])
+        if self.playerBoard[row][col].state == 'X':
+            if self.solutionBoard[row][col].isMine:
+                self.playerBoard[row][col].placeMine()
+                return False
             else:
-                self.playerBoard[row][col].state = self.solutionBoard[row][col].state
-            self.cellsRemaining -= 1
-            if self.cellsRemaining == self.num_mines:
-                return (True, True)
-        return (True, False)
+                if self.solutionBoard[row][col].state == '-':
+                    self.playerBoard[row][col].state = '-'
+                    self.autoOpen(row, col, [[False for x in range(self.dimension)] for y in range(self.dimension)])
+                else:
+                    self.playerBoard[row][col].state = self.solutionBoard[row][col].state
+            return True
+        return True
 
-    # def showSol(self):
-    #     # print('Mines remaining: ' + str(self.minesRemaining))
-    #     print('  ', end='')
-    #     for rowMarker in range(self.dimension):
-    #         print(rowMarker, end='')
-    #         print(' ', end='')
-    #     print('\n')
-    #     for row in range(self.dimension):
-    #         print(row, end='')
-    #         print(' ', end='')
-    #         for col in range(self.dimension):
-    #             print(self.solutionBoard[row][col].state, end='')
-    #             print(' ', end='')
-    #         print('\n')
+    def showSol(self):
+        # print('Mines remaining: ' + str(self.minesRemaining))
+        print('  ', end='')
+        for rowMarker in range(self.dimension):
+            print(rowMarker, end='')
+            print(' ', end='')
+        print('\n')
+        for row in range(self.dimension):
+            print(row, end='')
+            print(' ', end='')
+            for col in range(self.dimension):
+                print(self.solutionBoard[row][col].state, end='')
+                print(' ', end='')
+            print('\n')
 
-    def showPlaya(self):
+    def checkWin(self):
+        numLeft = 0
+        for row in range(0, self.dimension):
+            for col in range(0, self.dimension):
+                if self.playerBoard[row][col].state == 'X':
+                    numLeft += 1
+        if numLeft == self.num_mines:
+            return True
+        return False
+
+    def showPlayerBoard(self):
         # print('Mines remaining: ' + str(self.minesRemaining))
         print('  ', end='')
         for rowMarker in range(self.dimension):
@@ -125,6 +134,3 @@ class Board:
                 print(self.playerBoard[row][col].state, end='')
                 print(' ', end='')
             print('\n')
-
-
-    # TODO: Write interface
--- python/play.py
+++ python/play.py
@@ -3,34 +3,60 @@ import argparse
 
 
 def getUserInput(n):
-	print('Please enter a row and column in the following format:\n[row] [column]')
+	print("Please enter a move: ")
 	userRow, userCol = map(int, input().split())
 	# validate input
 	while (userRow < 0 or userRow >= n
 		or userCol < 0 or userCol >= n):
-		print('''
-				Oops! The row or column you entered is outside of the grid. Your row and column
-				must be between 0 and ''' + str(n))
+		print("Oops! The row or column you entered is outside of the grid. Your row and column must be between 0 and " + str(n))
+		print("Please enter a move: ")
 		userRow, userCol = map(int, input().split())
 	return userRow, userCol
 
-def main(n, num_mines):
-	print("Welcome to the greatest game ever hacked together by me!")
+def playgame(n, num_mines):
 	gameBoard = Board(n, num_mines)
-	gameBoard.showPlaya()
+	gameBoard.showSol()
+	gameBoard.showPlayerBoard()
 	userRow, userCol = getUserInput(n)
 	move = gameBoard.open(userRow, userCol)
-	while(move[0]):
+	while(move):
 		print('\n')
 		# gameBoard.showSol()
-		gameBoard.showPlaya()
+		gameBoard.showSol()
+		gameBoard.showPlayerBoard()
 		userRow, userCol = getUserInput(n)
 		move = gameBoard.open(userRow, userCol)
-		if move[1]:
-			print("WINNER")
-			break
-	gameBoard.showPlaya()
-	print("LOSER")
+		if gameBoard.checkWin():
+			return True
+	gameBoard.showPlayerBoard()
+	return False
+
+def printMenu():
+	print("Minesweeper 2k20")
+	print("All moves should be entered as follows:")
+	print("[row] [col]")
+	input("Press Enter to begin!")
+
+
+
+def main(n, num_mines):
+	printMenu()
+	choice = 'yes'
+	while choice == 'yes':
+		if playgame(n, num_mines):
+			print("Congrats, you win! Would you like to play again? (yes or no)")
+		else:
+			print("You've opened a mine :( Better luck next time! Would you like to play again? (yes or no)")
+		choice = str.lower(input())
+		while choice != 'yes' and choice != 'no':
+			print("Please enter 'yes' or 'no'")
+			choice = str.lower(input())
+	print("Thank you for playing Minesweeper 2k20. Have a great day!")
+
+
+
+
+	
 
 # DO NOT EDIT--------------------------------------------
 
