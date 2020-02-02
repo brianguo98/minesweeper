88805d859e19575bb01ac525a30a840665383660
Brian Guo <brianguo@umich.edu>
2020-02-01 21:34:08 -0500

    second commit

--- python/board.py
+++ python/board.py
@@ -1,9 +1,82 @@
+import random
+
+# Minesweeper
+
+
+class Cell:
+    def __init__(self, isMine=False):
+        self.state = 'X'
+        self.isMine = isMine
+    def placeMine(self):
+        self.isMine = True
+
+
+def placeMines(board, n, num_mines): 
+    '''randomly assigns num_mines mines to the n*n board'''
+    sequence = [i for i in range(n*n)]
+    mineLocations = set(random.sample(sequence, num_mines))
+    counter = 0
+    for row in range(n):
+        for col in range(n):
+            if counter in mineLocations:
+                board[row][col].placeMine()
+            counter += 1
+    return board
+
 class Board:
     def __init__(self, n, num_mines):
-        self.n = n
         self.num_mines = num_mines
+        self.cellsRemaining = n
+        self.n = n
+        self.board = placeMines([[Cell() for x in range(n*n)] for y in range(n*n)], n, num_mines)
+    
+    def countAdjacent(self, row, col):
+        counter = 0
+        for (adjRow, adjCol) in self.getAdjacent(row, col):
+            if (0 <= adjRow and adjRow < self.n
+                and 0 <= adjCol and adjCol < self.n
+                and self.board[adjRow][adjCol].isMine):
+                counter += 1
+        return counter
 
-        # TODO: Generate a random board 
-        raise NotImplementedError
+    def getAdjacent(self, row, col):
+        adjacent = []
+        surround = [(-1, -1), (-1, 0), (-1, 1),
+                    (0,-1),            (0, 1),
+                    (1, -1),  (1, 0),  (1, 1)]
+        for (adjRow, adjCol) in surround:
+            adjacent.append(((row+adjRow), (col+adjCol)))
+        return adjacent
+
+    def open(self, row, col):
+        if self.board[row][col].isMine:
+            self.board[row][col].state = 'M'
+            return (False, False)
+        else:
+            numAdj = self.countAdjacent(row, col)
+            if numAdj == 0:
+                self.board[row][col].state = '-'
+            else:
+                self.board[row][col].state = numAdj
+            self.cellsRemaining -= 1
+            if self.cellsRemaining == self.num_mines:
+                return (True, True)
+        return (True, False)
+
+    def show(self):
+        # print('Mines remaining: ' + str(self.minesRemaining))
+        print('  ', end='')
+        for rowMarker in range(self.n):
+            print(rowMarker, end='')
+            print(' ', end='')
+        print('\n')
+        for row in range(self.n):
+            print(row, end='')
+            print(' ', end='')
+            for col in range(self.n):
+                print(self.board[row][col].state, end='')
+                print(' ', end='')
+            print('\n')
+
 
     # TODO: Write interface
--- python/play.py
+++ python/play.py
@@ -1,9 +1,34 @@
 from board import Board
 import argparse
 
+
+def getUserInput(n):
+	print('Please enter a row and column in the following format:\n[row] [column]')
+	userRow, userCol = map(int, input().split())
+	# validate input
+	while (userRow < 0 or userRow >= n
+		or userCol < 0 or userCol >= n):
+		print('''
+				Oops! The row or column you entered is outside of the grid. Your row and column
+				must be between 0 and ''' + str(n))
+		userRow, userCol = map(int, input().split())
+	return userRow, userCol
+
 def main(n, num_mines):
-    # TODO: Write commandline game
-    raise NotImplementedError
+	print("Welcome to the greatest game ever hacked together by me!")
+	gameBoard = Board(n, num_mines)
+	gameBoard.show()
+	userRow, userCol = getUserInput(n)
+	move = gameBoard.open(userRow, userCol)
+	while(move[0]):
+		print('\n')
+		gameBoard.show()
+		userRow, userCol = getUserInput(n)
+		move = gameBoard.open(userRow, userCol)
+		if move[1]:
+			print("WINNER")
+			break
+	print("LOSER")
 
 # DO NOT EDIT--------------------------------------------
 
